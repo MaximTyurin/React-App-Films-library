2d5d579591734a85f3002a953a556398ef302e01
Максим <maxxxim54@mail.ru>
2022-01-30 16:59:41 +0300

    fix onKeyDown function

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,46 +1,47 @@
-import React, {useState} from 'react';
-import './Search.scss'
+import React, { useState } from 'react';
+import './Search.scss';
 
 const Search = (props) => {
-
-    const {
-        searchMovies = Function.prototype,
-    } = props;
+    const { searchMovies = Function.prototype } = props;
 
     const [search, setSearch] = useState('');
 
-    const handleKey = (event) => {
-        event.preventDefault();
-        if (event.key === 'Enter') {
+    const handleKey = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
             searchMovies(search);
         }
     };
 
     return (
-            <div className='row'>
-                <div className='input-field'>
-                    <input
-                        className='validate'
-                        id='email_inline'
-                        type='search'
-                        placeholder='Search'
-                        value={search}
-                        onChange={(event) =>
-                            setSearch(event.target.value)
-                        }
-                        onKeyDown={handleKey}
-                    />
-                    <button
-                        className='btn search-btn'
-                        onClick={() =>
-                            searchMovies(search)
-                        }
-                    >
-                        Search
-                    </button>
-                </div>
+        <div className='row'>
+            <div className='input-field'>
+                <input
+                    className='validate'
+                    type='search'
+                    placeholder='Search'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKey}
+                />
+
+                {/* <input
+                    className='validate'
+                    type='search'
+                    placeholder='Search'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={handleKey}
+                /> */}
+                <button
+                    className='btn search-btn'
+                    onClick={() => searchMovies(search)}
+                >
+                    Search
+                </button>
             </div>
-        );
-}
+        </div>
+    );
+};
 
 export default Search;
